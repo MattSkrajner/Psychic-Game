270e5bca80b4cce30a86f2f29dec9958a61f1604
MattSkrajner <matthew.skrajner@gmail.com>
2018-02-11 18:33:17 -0500

    update js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,9 +13,9 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 // Declare the variables
 var wins = 0;
 var losses = 0;
-var guessesLeft = 9;
-var guessesSoFar = [];
 var userGuess = null; // so userGuess does not have a value untila key is pressed
+var guessesSoFar = [];
+var guessesLeft = 9;
 
 // Make computer randomly pick a letter from computerChoices array
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
